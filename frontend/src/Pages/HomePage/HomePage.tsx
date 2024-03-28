facd50db2e7a7f6d7824328f4e538d20021fc518
arshadakl <arshadayanikkal@gmail.com>
2024-03-28 15:33:41 +0530

    nav bar manage dynamicly

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,31 @@
-import HeroSection from "../../components/HomeComponents/HeroSection"
-import Popular from "../../components/HomeComponents/Popular"
-import NavBar from "../../components/Navbar/NavBar"
+import { useEffect, useState } from "react";
+import HeroSection from "../../common/components/HomeComponents/HeroSection"
+import Popular from "../../common/components/HomeComponents/Popular"
+import NavBar from "../../common/components/Navbar/NavBar"
 
 
 function HomePage() {
+  const [scrolled, setScrolled] = useState<Boolean>(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+      if (scrollPercentage >= 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
-    <NavBar fixed={"top"} bg={"none"}/>
+    <NavBar fixed={"top"} bg={scrolled ? "dark" : "none"}/>
     <HeroSection/>
     <Popular/>
     </>
